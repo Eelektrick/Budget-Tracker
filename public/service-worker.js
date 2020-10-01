98a077e1d88f4471ac21e790a003a3dd843ccfbe
Corey Post <corey.post0120@gmail.com>
2020-09-30 22:31:31 -0600

    typo with files to cache fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     "/db.js",
     "/styles.css",
     "/index.js",
-    "/manifest.json",
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
 ];
